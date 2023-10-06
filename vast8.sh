68bcd295862c3e5b9e7b4b1beb2f37e0978050df
Crypto4life777 <147115811+Crypto4life777@users.noreply.github.com>
2023-10-06 02:57:16 +0200

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x81685942DE961979deB46183862F1eD684D533d4/g' config.conf
 sleep 5
 echo "---------------------------"
 
